5dd9fa3e7250e643100549a7f7892e0e615bb78f
maahjoub <am470593@gmail.com>
2022-03-17 14:13:58 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let image = document.querySelectorAll('.pro-img img')
+let body = document.querySelector(".projects")
+image.forEach((img) => {
+    img.addEventListener('click', (e) => {
+        // e.target.style.width = '500px' 
+    })
+})
+
